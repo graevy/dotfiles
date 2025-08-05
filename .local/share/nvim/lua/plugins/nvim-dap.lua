bdcc7fc866d292ebc8e3dd853df577c46af5fed2
avery <avry@pm.me>
2025-08-04 19:02:16 -0700

    python for nvim

--- .local/share/nvim/lua/plugins/nvim-dap.lua
+++ .local/share/nvim/lua/plugins/nvim-dap.lua
@@ -3,12 +3,8 @@ return {
   event = "VeryLazy",
   config = function()
     local dap = require("dap")
-    -- dap.adapters.gdb = {
-    --   type = "executable",
-    --   command = "gdb",
-    --   args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
-    --   name = "gdb"
-    -- }
+
+    -- adapters
     dap.adapters.lldb = {
       type = "server",
       port = "${port}",
@@ -18,6 +14,13 @@ return {
       }
     }
 
+    dap.adapters.python = {
+      type = 'executable',
+      command = 'python',
+      args = { '-m', 'debugpy.adapter' },
+    }
+
+    -- configs
     dap.configurations.rust = {
       {
         name = "lldbLaunch",
@@ -39,56 +42,19 @@ return {
         cwd = vim.fn.getcwd(),
         stopOnEntry = false,
       },
-      -- {
-      --   name = "gdbLaunch",
-      --   type = "gdb",
-      --   request = "launch",
-      --   program = function()
-      --     return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
-      --   end,
-      --   cwd = vim.fn.getcwd(),
-      --   stopOnEntry = false,
-      -- },
-      -- {
-      --   name = "gdbLaunch with Args",
-      --   type = "gdb",
-      --   request = "launch",
-      --   program = function()
-      --     return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
-      --   end,
-      --   cwd = vim.fn.getcwd(),
-      --   stopOnEntry = false,
-      --   args = function()
-      --     return vim.split(vim.fn.input("Arguments: ", "", "file"), " ")
-      --   end,
-      -- },
-      -- {
-      --   name = "Select and attach to process",
-      --   type = "gdb",
-      --   request = "attach",
-      --   program = function()
-      --     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-      --   end,
-      --   pid = function()
-      --     local name = vim.fn.input('Executable name (filter): ')
-      --     return require("dap.utils").pick_process({ filter = name })
-      --   end,
-      --   cwd = '${workspaceFolder}'
-      -- },
-      -- {
-      --   name = 'Attach to gdbserver :1234',
-      --   type = 'gdb',
-      --   request = 'attach',
-      --   target = 'localhost:1234',
-      --   program = function()
-      --     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-      --   end,
-      --   cwd = '${workspaceFolder}'
-      -- },
     }
 
-    -- this is bad because of the target/debug thang, gotta fix
-    -- dap.configurations.c = dap.configurations.rust
-    -- dap.configurations.cpp = dap.configurations.rust
+    dap.configurations.python = {
+      {
+        type = 'python',
+        request = 'launch',
+        name = "Launch file",
+        program = "${file}",
+        pythonPath = function()
+          return vim.fn.exepath('python3') or 'python'
+        end,
+      },
+    }
+
   end,
 }
