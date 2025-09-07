8c5b9a684aed1d438010f89976672a4ddd0ffbb6
avery <avry@pm.me>
2025-09-06 17:58:21 -0700

    music flake good

--- flakes/music/flake.nix
+++ flakes/music/flake.nix
@@ -1,26 +1,26 @@
 {
   description = "for processing music";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }: 
-    let
-      system = builtins.currentSystem;
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.kid3-cli
-          pkgs.flacon
-          pkgs.monkeysAudio
-          pkgs.sox
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShells.default = nixpkgs.legacyPackages.${system}.mkShell {
+        buildInputs = with nixpkgs.legacyPackages.${system}; [
+          kid3-cli
+          flacon
+          monkeysAudio
+          sox
         ];
 
-	# flacon likes to write to `.config/flacon/`
-	# why does flacon need state. also it inadvertently version-pins nixpkgs
-	# anyway just give it a black hole to dump its state into
-	shellHook = ''export XDG_CONFIG_HOME=$(mktemp -d)'';
+        # flacon likes to write to `.config/flacon/`
+        # why does flacon need state. also it inadvertently version-pins nixpkgs
+        # anyway just give it a black hole to dump its state into
+        shellHook = ''export XDG_CONFIG_HOME=$(mktemp -d)'';
       };
-    };
+    });
 }
 
