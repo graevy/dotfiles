f730e895655d66c7d1bca5c5425c676ef3bbb5d0
io <io@cute.tg>
2025-10-02 22:01:12 -0700

    go support

--- .local/share/nvim/lua/plugins/nvim-dap.lua
+++ .local/share/nvim/lua/plugins/nvim-dap.lua
@@ -20,6 +20,15 @@ return {
       args = { '-m', 'debugpy.adapter' },
     }
 
+    dap.adapters.go = {
+      type = "server",
+      port = "${port}",
+      executable = {
+        command = "dlv",
+        args = { "dap", "-l", "127.0.0.1:${port}" },
+      },
+    }
+
     -- configs
     dap.configurations.rust = {
       {
@@ -53,6 +62,28 @@ return {
         pythonPath = function()
           return vim.fn.exepath('python3') or 'python'
         end,
+      },
+    }
+
+    dap.configurations.go = {
+      {
+        type = "go",
+        name = "Debug",
+        request = "launch",
+        program = "${file}",
+      },
+      {
+        type = "go",
+        name = "Debug Package",
+        request = "launch",
+        program = "${fileDirname}",
+      },
+      {
+        type = "go",
+        name = "Attach",
+        mode = "local",
+        request = "attach",
+        processId = require("dap.utils").pick_process,
       },
     }
 
