31e1c05351049fd30acb413923047c067e1d373f
io <io@cute.tg>
2026-03-27 02:13:09 -0700

    rotating some hashes. hmm. maybe gitignore time

--- flakes/pokerogue/flake.nix
+++ flakes/pokerogue/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Standalone development shell for PokeRogue with GPU support";
+  description = "pokerogue development shell w/ gpu support";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -24,27 +24,25 @@
             export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
               pkgs.libGL
               pkgs.libglvnd
-              pkgs.mesa.drivers
+              pkgs.mesa
               pkgs.vulkan-loader
               pkgs.libxkbcommon
               pkgs.wayland
-              pkgs.xorg.libX11
-              pkgs.xorg.libXcursor
-              pkgs.xorg.libXrandr
-              pkgs.xorg.libXi
-              pkgs.xorg.libxcb
-              pkgs.xorg.libXcomposite
-              pkgs.xorg.libXdamage
-              pkgs.xorg.libXext
-              pkgs.xorg.libXfixes
+              pkgs.libX11
+              pkgs.libXcursor
+              pkgs.libXrandr
+              pkgs.libXi
+              pkgs.libxcb
+              pkgs.libXcomposite
+              pkgs.libXdamage
+              pkgs.libXext
+              pkgs.libXfixes
               pkgs.stdenv.cc.cc.lib
             ]}:$LD_LIBRARY_PATH"
             
-            export LIBGL_DRIVERS_PATH="${pkgs.mesa.drivers}/lib/dri"
-            export __EGL_VENDOR_LIBRARY_DIRS="${pkgs.mesa.drivers}/share/glvnd/egl_vendor.d"
-            export __GLX_VENDOR_LIBRARY_DIRS="${pkgs.mesa.drivers}/share/glvnd/glx_vendor.d"
-            
-            echo "Run 'pokerogue' to start"
+            export LIBGL_DRIVERS_PATH="${pkgs.mesa}/lib/dri"
+            export __EGL_VENDOR_LIBRARY_DIRS="${pkgs.mesa}/share/glvnd/egl_vendor.d"
+            export __GLX_VENDOR_LIBRARY_DIRS="${pkgs.mesa}/share/glvnd/glx_vendor.d"
           '';
         };
       }
