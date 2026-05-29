b33c74a0eac05bebe8261be9a5141139470a7da5
io <io@cute.tg>
2026-05-28 23:06:51 -0700

    just set coord xform matrix directly for mouse

--- .config/i3/mouse.sh
+++ .config/i3/mouse.sh
@@ -6,6 +6,10 @@ device="SOAI USB Gaming Mouse"
 id=$(xinput list --id-only "$device" 2>/dev/null)
 
 if [ -n "$id" ]; then
-	xinput set-prop "$id" "libinput Accel Speed" -1.0
+	# xinput set-prop "$id" "libinput Accel Speed" -1.0
+	xinput set-prop "$id" "Coordinate Transformation Matrix" \
+		1 0 0 \
+		0 1 0 \
+		0 0 7
 fi
 
