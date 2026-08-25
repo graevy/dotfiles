8792a3f5fce4b2a4e9cdf9c5e4580a512d80516d
io <io@cute.tg>
2026-08-24 22:45:10 -0700

    actually need nui here

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -5,7 +5,7 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
-      -- "MunifTanjim/nui.nvim",
+      "MunifTanjim/nui.nvim",
       -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     lazy = true,
